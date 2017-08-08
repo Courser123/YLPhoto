a6caaded84fc9804f750baed8060bdfa1da31eb8
Courser <wangzhongdi@meituan.com>
2017-08-08 19:56:58 +0800

    create

---
+++ YLPhoto/YLPhoto/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YLPhoto
+//
+//  Created by Courser on 08/08/2017.
+//  Copyright © 2017 Courser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YLPhoto/YLPhoto/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YLPhoto
+//
+//  Created by Courser on 08/08/2017.
+//  Copyright © 2017 Courser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
